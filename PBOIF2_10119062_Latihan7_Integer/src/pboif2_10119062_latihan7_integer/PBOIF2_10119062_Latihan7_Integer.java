50bc8c0c5fe64dffc362f5f7e51cf507ce3fbcd3
NaufalRafiPratama <73165365+NaufalRafiPratama@users.noreply.github.com>
2020-10-20 17:23:16 +0700

    Add files via upload

---
+++ PBOIF2_10119062_Latihan7_Integer/src/pboif2_10119062_latihan7_integer/PBOIF2_10119062_Latihan7_Integer.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2_10119062_latihan7_integer;
+
+/**
+ *
+ * @author 
+ * NAMA     : Naufal Rafi Pratama
+ * KELAS    : PBOIF2
+ * NIM      : 10119062
+ * Deksripsi Program : Program ini berisi program untuk menampilkan jenis-jenis
+ * tipe data bilangan bulat 
+ */
+public class PBOIF2_10119062_Latihan7_Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int: " + i);
+        System.out.println("long : " + l);
+    }
+    
+}
